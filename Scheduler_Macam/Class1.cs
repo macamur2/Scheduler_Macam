16cf5bc42684c16e0ca31245ae56f68c2fc0b7dc
macamur2 <macamur2@gmail.com>
2021-10-13 16:46:59 +0200

    Estructura básica incompleta v.01

--- Scheduler_Macam/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Scheduler_Macam
-{
-    public class Class1
-    {
-        //Prueba de commit
-    }
-}
---
+++ Scheduler_Macam/Schedule/Schedule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business_Layer
+{
+    /// <summary>
+    /// Schedule Class RELLENAR
+    /// </summary>
+    public class Schedule
+    {
+        public Schedule()
+        {
+
+        }
+
+        #region Properties
+        public DateTime CurrentDate { get; set; }
+
+        #endregion
+    }
+}
